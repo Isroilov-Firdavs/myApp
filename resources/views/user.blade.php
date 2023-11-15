c4ced621a2e184f41403d8ec0db09cfa972bb77a
Isroilov-Firdavs <firdavsisroilov28@gmail.com>
2023-11-15 16:50:01 +0500

    Route group

---
+++ resources/views/user.blade.php
@@ -0,0 +1,2 @@
+<h1>USER ID: <?php echo $id;?></h1>
+<h1>USER ID: <?php echo $name;?></h1>
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/users', function () {
-    return view('users');
-});
-Route::get('/services/test', function () {
-    return view('services');
+
+Route::prefix('admin')->name('admin.')->group(function(){
+    Route::get('/users', function(){
+        return view('users');
+    })->name('users');
 });
